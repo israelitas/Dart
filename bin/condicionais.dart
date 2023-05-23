77e6db88c4d25c9c9b1273a06a0801889d0e82fc
israelitas <israel.andreotti01@gmail.com>
2023-05-23 14:59:04 -0300

    condicionais e repeticao

---
+++ bin/condicionais.dart
@@ -0,0 +1,17 @@
+void main () {
+  int idade = 21;
+  bool maiorDeIdade;
+
+  if(idade >= 18) {
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+
+  print("Eu sou maior de idade? $maiorDeIdade");
+
+}
+
+
+
+
---
+++ bin/projeto_inicial.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  int idade = 21;
+  double altura = 1.77;
+  bool geek = false;
+  const String nome = "Israel Andreotti";
+  final String apelido = "Isra";
+  var time = "inter";
+
+  List<dynamic> israel = [idade, altura, nome, apelido, geek, time];
+
+  print(israel);
+
+  print("Meu apelido é ${israel[3]} ");
+  print("Meu nome é ${israel[2]}");
+  print("Tenho ${israel[0]} anos");
+  print("Tenho ${israel[1]} de altura");
+  print("Eu torço (infelizmente) para o ${israel[5]}");
+
+}
